0e7752f5f6aba742b259f17aee41ec38abd006c6
nasir <nasir.dev74@gmail.com>
2022-12-10 02:03:37 +0600

    Initial Commit

---
+++ SweaterShop/SweaterShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopApp.swift
+//  SweaterShop
+//
+//  Created by Softcent on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
